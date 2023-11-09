b0d10eeb1bd5699ef7bcb78929f3bd24c4c549a4
tiarapsh1016 <110644545+tiarapsh1016@users.noreply.github.com>
2023-11-09 09:08:20 +0700

    keranjang.php

--- keranjang.php
+++ keranjang.php
@@ -3,16 +3,6 @@
     include "koneksi.php";
     session_start();
 
-    if (isset($_GET['remove'])) {
-        $remove_id = $_GET['remove'];
-        mysqli_query($conn, "DELETE FROM `keranjang` WHERE id = '$remove_id'");
-        header('location:keranjang.php');
-     };
-     
-     if (isset($_GET['delete_all'])) {
-        mysqli_query($conn, "DELETE FROM `keranjang`");
-        header('location:keranjang.php');
-     }
 ?>
  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
  <br><br><br><br><br>
@@ -28,23 +18,39 @@
         </tr>
     </thead>
     <tbody>
+    <tbody>
         <?php
-        
-        foreach ($_SESSION['keranjang']as $id_menu => $jumlah): 
-            // $ambil = mysqli_query($conn, "SELECT * FROM menu");
-            //   $pecah = $ambil -> fetch_assoc();
-        ?>
+        if (isset($_SESSION['keranjang']) && is_array($_SESSION['keranjang'])) {
+            foreach ($_SESSION['keranjang'] as $key_produk => $val_produk):
+                ?>
+                <tr>
+                    <td>
+                        <?= ($key_produk + 1) ?>
+                    </td>
+                    <td>
+                        <?= $val_produk['nama_menu'] ?>
+                    </td>
+                    <td>
+                        <?= $val_produk['jumlah_order'] ?>
+                    </td>
+                    <td>
+                        $
+                        <?= $val_produk['harga_pcs'] * $val_produk['jumlah_order'] ?>
+                    </td>
+                    <td><a href="hapus_keranjang.php?id=<?= $key_produk ?>" class="btn btn-danger"><strong>X</strong></a></td>
+                </tr>
+            <?php endforeach ?>
+            <?php
+        } else {
+            ?>
             <tr>
-                <td><?=($id_menu+1)?></td>
-                <td><?=$jumlah['nama_menu']?></td>
-                <td><?=$jumlah['jumlah_order']?></td>
-                <td>$<?=$jumlah['harga_pcs']?></td>
-                <td><a href="cart.php?remove=<?php echo $fetch_cart['id']; ?>" onclick="return confirm('Hapus menu dari pesanan?')" class="btn btn-danger btn sm"> </i> Hapus</a></td>
+                <td colspan="5">Your cart is empty.</td>
             </tr>
+        <?php
+        }
+       ?>
+       </tbody>
 
-        <?php 
-        endforeach ?>
-    </tbody>
 
                
 
